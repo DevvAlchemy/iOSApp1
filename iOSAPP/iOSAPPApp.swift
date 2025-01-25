ec9e0d910dec4df76ed9667bb95f51593ea605ba
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-01-25 13:40:37 -0500

    Initial Commit

---
+++ iOSAPP/iOSAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAPPApp.swift
+//  iOSAPP
+//
+//  Created by Royal K on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct iOSAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSAPPTests/iOSAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAPPTests.swift
+//  iOSAPPTests
+//
+//  Created by Royal K on 2025-01-25.
+//
+
+import Testing
+@testable import iOSAPP
+
+struct iOSAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
